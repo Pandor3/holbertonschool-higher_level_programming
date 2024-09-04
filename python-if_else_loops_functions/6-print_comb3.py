d2b68319a60cb0d801ca5b1dab970eba9e8176e9
Pandor3 <thatdumbphilosopher@gmail.com>
2024-09-04 12:05:46 +0000

    Added 6-print_comb3.py file, edited README.md page

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
 
-for i in range(100)
-    print(
+for i in range(0, 10):
+    for j in range(i + 1, 10):
+        if i != 0 or j != 1:
+            print(", ", end="")
+        print(f"{i}{j}", end="")
+print()
