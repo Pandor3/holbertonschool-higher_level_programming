37d8e920514f372547e0c1ab9bb78a26ae18043e
Pandor3 <thatdumbphilosopher@gmail.com>
2024-09-04 08:21:06 +0000

    Edited 1-last_digit.py, changed syntax

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -11,5 +11,5 @@ if last_digit > 5:
     print("and is greater than 5")
 elif last_digit == 0:
     print("and is 0")
-else:
+elif last_digit < 6 and last_digit < 0:
     print("and is less than 6 and 0")
