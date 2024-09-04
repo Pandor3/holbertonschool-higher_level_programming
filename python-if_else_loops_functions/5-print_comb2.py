f52bdb9c39bd5e9f810db7a9ca895e5318252cab
Pandor3 <thatdumbphilosopher@gmail.com>
2024-09-04 09:04:41 +0000

    Edited exercise 5, changed syntax

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
+
 for i in range(00, 100):
-    if i in range(1, 100):
+    if i > 0:
         print(", ", end="")
-    print(f"{i}", end="")
+    print(f"{i:02}", end="")
 print()
