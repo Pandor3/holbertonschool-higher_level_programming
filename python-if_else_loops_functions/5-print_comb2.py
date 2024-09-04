1dcda6c7812c0ea2875c13b6e07136ec07970ff6
Pandor3 <thatdumbphilosopher@gmail.com>
2024-09-04 09:14:22 +0000

    Edited exercise 5 to pass the checker.

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
 
 for i in range(00, 100):
-    if i > 0:
-        print(", ", end="")
-    print(f"{i:02}", end="")
-    
-print()
+    print(f"{i:02}", end=", " if i < 99 else "\n")
