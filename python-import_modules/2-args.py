c945de7cde5b368e30d09b47078d2d0e1d1d99fd
Pandor3 <thatdumbphilosopher@gmail.com>
2024-09-05 11:57:29 +0000

    Added 2nd exercise

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import sys
+
+if __name__ == "__main__":
+    argv = sys.argv
+    num_args = len(argv) - 1
+
+    if num_args == 0:
+        print("0 arguments.")
+    elif num_args == 1:
+        print("1 arguments:")
+    else:
+        print("{} arguments:".format(num_args))
+
+    for i in range(1, len(argv)):
+        print("{}: {}".format(i, argv[i]))
