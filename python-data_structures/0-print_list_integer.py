032c6b32e993b73e647f65c01ff9da6ed233b8cb
Pandor3 <thatdumbphilosopher@gmail.com>
2024-09-09 15:35:50 +0000

    Edited exercise 0 and 3 in accordance to the requirements

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 
 def print_list_integer(my_list=[]):
-    print(f"{my_list[0]}")
-    print(f"{my_list[1]}")
-    print(f"{my_list[2]}")
-    print(f"{my_list[3]}")
-    print(f"{my_list[4]}")
+    for i in my_list:
+        print("{:d}".format(i))
--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 
 def print_reversed_list_integer(my_list=[]):
-    print(f"{my_list[-1]}")
-    print(f"{my_list[-2]}")
-    print(f"{my_list[-3]}")
-    print(f"{my_list[-4]}")
-    print(f"{my_list[-5]}")
+    for i in reversed(my_list):
+        print("{:d}".format(i))
