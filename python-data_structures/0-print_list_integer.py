6c8c48dcf264ad0142789703025f721fc0814e05
Pandor3 <thatdumbphilosopher@gmail.com>
2024-09-09 14:52:23 +0000

    Added exercise 0 and its main file

---
+++ python-data_structures/0-print_list_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def print_list_integer(my_list=[]):
+    print(f"{my_list[0]}")
+    print(f"{my_list[1]}")
+    print(f"{my_list[2]}")
+    print(f"{my_list[3]}")
+    print(f"{my_list[4]}")
