5e392617219d2efd835ae808d83f55a556fbc9ef
Pandor3 <thatdumbphilosopher@gmail.com>
2024-09-09 15:26:44 +0000

    Added exercise 2 and 3 aswell as their main files

---
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def print_reversed_list_integer(my_list=[]):
+    print(f"{my_list[-1]}")
+    print(f"{my_list[-2]}")
+    print(f"{my_list[-3]}")
+    print(f"{my_list[-4]}")
+    print(f"{my_list[-5]}")
