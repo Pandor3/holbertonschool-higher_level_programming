7969eed771fdf95634ba9f6baae19b4e291e0111
Pandor3 <thatdumbphilosopher@gmail.com>
2024-09-10 07:37:59 +0000

    Edited exercise 8

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -2,5 +2,6 @@
 
 def multiple_returns(sentence):
     if len(sentence) == 0:
-        sentence[0] = None
-    return len(sentence), sentence[0]
+        return None
+    else:
+        return len(sentence), sentence[0]
