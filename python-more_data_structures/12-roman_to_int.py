bb6ed2407b9ad740e3318f4cead5367e41392dac
Pandor3 <thatdumbphilosopher@gmail.com>
2024-09-11 11:11:54 +0000

    Edited exercise 12 due to requirement fail

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def roman_to_int(roman_string):
-    if not  isinstance(roman_string, str) or roman_string == None:
+    if not isinstance(roman_string, str) or roman_string == None:
         return 0
     roman_dictionary = {
             'I': 1, 'V': 5, 'X': 10, 'L': 50,
