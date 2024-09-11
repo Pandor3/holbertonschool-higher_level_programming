156d28c85e1a7cec904734669e02b08680e6994f
Pandor3 <thatdumbphilosopher@gmail.com>
2024-09-11 12:17:25 +0000

    Edited exercise 1 due to syntax problems

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 
 def safe_print_integer(value):
-    count = 0
     try:
         if isinstance(value, int):
             print("{:d}".format(value))
-            count = 1
+            return True
     except (TypeError, ValueError):
-        pass
-    return count
+        return False
