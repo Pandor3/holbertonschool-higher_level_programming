f578cf39902ec4dccd1d6b810aaf95855cb3d306
Pandor3 <thatdumbphilosopher@gmail.com>
2024-09-11 12:19:48 +0000

    Edited exercise 1 due to a requirement fail

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -2,7 +2,8 @@
 
 def safe_print_integer(value):
     try:
-            print("{:d}".format(value))
-            return True
+        print("{:d}".format(value))
+        return True
     except (TypeError, ValueError):
         return False
+    print()
