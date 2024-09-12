30ce7f590795994dac4ff00519d3e3aff9cce1cf
Pandor3 <Thatdumbphilosopher@gmail.com>
2024-09-12 09:47:16 +0000

    Edited exercise 0 files

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,3 +1,14 @@
 #!/usr/bin/python3
 
 def add_integer(a, b=98):
+    result = a + b
+    try:
+        print("{}".format(result))
+        if not isinstance(a(int, float)):
+            raise TypeError ("a must be an integer")
+        if not isinstance(b(int, float)):
+            raise TypeError ("b must be an integer")
+    except ValueError:
+        return result
+    finally:
+        return result
