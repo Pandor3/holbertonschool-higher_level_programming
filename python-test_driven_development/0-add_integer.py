2977db5b2924522b4b079be4820a40da079a698c
Pandor3 <Thatdumbphilosopher@gmail.com>
2024-09-12 10:47:30 +0000

    Edited exercise 0 files

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
 
 def add_integer(a, b=98):
-    result = a + b
-    try:
-        print("{}".format(result))
-        if not isinstance(a(int, float)):
-            raise TypeError ("a must be an integer")
-        if not isinstance(b(int, float)):
-            raise TypeError ("b must be an integer")
-    except ValueError:
-        return result
-    finally:
-        return result
+    """
+    This function returns the addition of two numbers
+    after checking their types of course
+
+    a and b must be integers or floats, else a TypeError message will appear
+    if they are floats then they'll be converted to integers.
+    """
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+    a = int(a)
+    b = int(b)
+    return a + b
