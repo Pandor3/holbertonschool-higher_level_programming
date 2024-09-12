e8e6b7b3a004da34599ff11322575aa04a80d7ed
Pandor3 <Thatdumbphilosopher@gmail.com>
2024-09-12 17:44:40 +0000

    Added exercise 3 and its main files aswell as the doctest file

---
+++ python-test_driven_development/4-print_square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+
+"""
+This contains the print_size function
+"""
+
+
+def print_square(size):
+
+
+    """
+Prints:
+    Size Square
+Size:
+    Value of the print
+    """
+
+
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    for _ in range(size):
+        print("#" * size)
