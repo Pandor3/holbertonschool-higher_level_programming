1c08235007160072f6964898c6da8cf1383e41d7
Pandor3 <Thatdumbphilosopher@gmail.com>
2024-09-12 14:45:15 +0000

    Edited exercise 2

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,6 +1,20 @@
 #!/usr/bin/python3
 
+"""
+This contains the say_my_name function which prints a name and last name
+"""
+
 def say_my_name(first_name, last_name=""):
+
+    """
+    Parameters:
+    First_name which is a string containing the first name
+    Last_name which is a string containing the last name
+
+    Raises:
+    TypeError is first_name or last_name are not a string
+    """
+
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
