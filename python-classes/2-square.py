2263639a6193b2309e4d5a2aca7b707e4ebdcdb1
Pandor3 <thatdumbphilosopher@gmail.com>
2024-09-17 12:46:52 +0000

    Added documentation to exercise 2

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -8,10 +8,12 @@ This module will be used to:
 
 
 class Square:
+    """
+    This class is used to define a square.
+    """
     def __init__(self, size=0):
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
-    
