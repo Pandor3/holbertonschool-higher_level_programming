535ab0bab8f12155afba717e1da7b4e1e30f85d3
Pandor3 <thatdumbphilosopher@gmail.com>
2024-09-17 12:28:29 +0000

    Added exercise 1 and its main file

---
+++ python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+This module will be used to define the square class
+And  define its size
+"""
+
+
+class Square:
+    """
+This class is used to define a square
+    """
+
+    def __init__(self, size):
+        self.__size = size
