de4b231898224d252cb2b6d263bfd6851679aacc
Pandor3 <thatdumbphilosopher@gmail.com>
2024-09-18 15:38:42 +0000

    Edited exercise 7, made a string into a list

--- python-more_classes/7-rectangle.py
+++ python-more_classes/7-rectangle.py
@@ -64,7 +64,7 @@ class Rectangle:
         lines = []
         for _ in range(self.height):
             if isinstance(self.print_symbol, list):
-                row = ''.join(str(symbol) for symbol in self.print_symbol) * self.width
+                row = ''.join("{}".format(self.print_symbol) for _ in range(self.width))
             else:
                 row = str(self.print_symbol) * self.__width
             lines.append(row)
