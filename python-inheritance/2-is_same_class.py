36d612db9548b68317d1be23e4766d7b3a9f1015
Pandor3 <thatdumbphilosopher@gmail.com>
2024-09-23 13:31:40 +0000

    Added exercise 2 and its main file

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+This module will contain a function
+which returns true or false
+"""
+
+
+def is_same_class(obj, a_class):
+    """
+    This defined function will be used
+    to return true or false
+    """
+
+    if isinstance(obj, a_class):
+        return (True)
+    else:
+        return (False)
