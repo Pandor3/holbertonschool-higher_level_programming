044ac1695ff5fd473ecd4c2015f4de20121d5f80
Pandor3 <thatdumbphilosopher@gmail.com>
2024-09-23 14:32:49 +0000

    Added exercise 6 and its main file

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+This module will contain a class BaseGeometry
+with a public instance method about its area
+"""
+
+
+class BaseGeometry:
+    """
+    A class called BaseGeometry
+    """
+
+    def area(self):
+        """
+        A defined function which will raise
+        an error message
+        """
+
+        raise Exception("area() is not implemented")
