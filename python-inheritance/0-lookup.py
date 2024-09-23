d5b3257c3bc9c2c96baaf36c9ec3175cf27a203d
Pandor3 <thatdumbphilosopher@gmail.com>
2024-09-23 13:01:50 +0000

    Added exercise 0 and its main file

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+This module will be used to contain
+the lookup function
+"""
+
+
+def lookup(obj):
+    """
+    This define the lookup function
+    """
+
+    return dir(obj)
