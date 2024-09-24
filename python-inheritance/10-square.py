63883b00f9c67a7e4317a2281b578c0fde5c1833
Pandor3 <thatdumbphilosopher@gmail.com>
2024-09-24 11:22:44 +0000

    Added exercise 10 and its main file

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+This module will contain a Square with its
+various parameters
+"""
+
+Rectangle = __import__('8-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+    This class contains a Square
+    """
+
+    def __init__(self, size):
+        """
+        A constructor which will initialize the size of the
+        square
+        """
+
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        """
+        A defined function which will print and return
+        the area of a square
+        """
+
+        print("[Rectangle] {}/{}".format(self.__size, self.__size))
+        return self.__size * self.__size
