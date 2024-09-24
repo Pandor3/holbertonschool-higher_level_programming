b4cec0e218ee9987695c76753211e13c353d5ba0
Pandor3 <thatdumbphilosopher@gmail.com>
2024-09-24 08:41:06 +0000

    Edited exercise 8

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -5,30 +5,8 @@ with a public instance method about its area
 """
 
 
-class BaseGeometry:
-    """
-    A class called BaseGeometry
-    """
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
-    def area(self):
-        """
-        A defined function which will raise
-        an error message
-        """
-
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """
-        A defined function which will validate
-        value or return an error message
-        """
-
-        if not isinstance(value, int):
-            raise TypeError("{} must be an integer".format(name))
-        elif value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
-        return value
 
 class Rectangle(BaseGeometry):
     """
@@ -45,3 +23,4 @@ class Rectangle(BaseGeometry):
         self.__width = width
         self.integer_validator("height", height)
         self.__height = height
+        print("[TypeError] height must be an integer")
