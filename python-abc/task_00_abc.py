d581bfe36f9783e4d087c6e28818367af1e00ac2
Pandor3 <thatdumbphilosopher@gmail.com>
2024-09-25 08:59:24 +0000

    Added exercise 0 and its main files

--- python-abc/task_00_abc.py
+++ python-abc/task_00_abc.py
@@ -1,1 +1,39 @@
 #!/usr/bin/python3
+"""
+This module will be used to return the sounds of
+various animals
+"""
+
+
+from abc import ABC, abstractmethod
+
+
+class Animal(ABC):
+    """
+    Animal class
+    """
+
+    @abstractmethod
+    def sound(self):
+        """
+        A defined function which will produce
+        a sound
+        """
+
+        pass
+
+
+class Dog(Animal):
+    """
+    Dog class
+    """
+    def sound(self):
+        return "Bark"
+
+
+class Cat(Animal):
+    """
+    Cat class
+    """
+    def sound(self):
+        return "Meow"
