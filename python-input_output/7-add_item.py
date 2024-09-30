d89fa3666a61dedda1648ece1895167da8a39346
Pandor3 <thatdumbphilosopher@gmail.com>
2024-09-30 12:25:31 +0000

    Added exercise 7 and its main file, should ask more during the PLD about it

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -5,16 +5,18 @@ all arguments to a Python list and then save them in a file
 """
 
 
+import os
 import sys
 import json
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
-args = sys.argv
 filename = "add_item.json"
-with open(filename, 'a+', encoding="utf-8") as f:
-    my_list = []
-    my_list.extend(args[1:])
-    save_to_json_file(my_list, filename)
-    load_from_json_file(filename)
+
+if os.path.exists(filename):
+    items = load_from_json_file(filename)
+else:
+    items = []
+items.extend(sys.argv[1:])
+save_to_json_file(items, filename)
