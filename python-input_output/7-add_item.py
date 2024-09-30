4e74acf9519358b25031050c34f0cb5ca75fe973
Pandor3 <thatdumbphilosopher@gmail.com>
2024-09-30 12:10:20 +0000

    Added exercise 7 and its main file

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+This module will be used to contain a function which will add
+all arguments to a Python list and then save them in a file
+"""
+
+
+import sys
+import json
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+
+args = sys.argv
+filename = "add_item.json"
+with open(filename, 'a+', encoding="utf-8") as f:
+    my_list = []
+    my_list.extend(args[1:])
+    save_to_json_file(my_list, filename)
+    load_from_json_file(filename)
