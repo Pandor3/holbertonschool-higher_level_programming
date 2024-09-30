e261dc529bda867015d83bea3c1cb2f1be29f983
Pandor3 <thatdumbphilosopher@gmail.com>
2024-09-30 13:21:48 +0000

    Edited exercise 8, removed json import

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -4,8 +4,6 @@ This module will contain a function which returns the dictionary description
 for JSON serialization of an object
 """
 
-import json
-
 
 def class_to_json(obj):
     return obj.__dict__
