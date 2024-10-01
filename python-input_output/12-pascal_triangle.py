2642f6c73c56114ccc01ac30d1d6c961df711e74
Pandor3 <thatdumbphilosopher@gmail.com>
2024-10-01 09:08:24 +0000

    Added exercise 12 and its main file

---
+++ python-input_output/12-pascal_triangle.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+This module will be used to contain a function which will
+return a list of list of integers representing a Pascal Triangle
+"""
+
+
+def pascal_triangle(n):
+    n_list = []
+    if n <= 0:
+        return n_list
+
+    for x in range(n):
+        list_added = [1] * (x + 1)
+        if x > 1:
+            for y in range(1, x):
+                list_added[y] = n_list[x - 1][y - 1] + n_list[x - 1][y]
+        n_list.append(list_added)
+
+    return n_list
