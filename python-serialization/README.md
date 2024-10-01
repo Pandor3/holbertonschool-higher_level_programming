913cbb50cec903807d134d0e5b463228e83efd73
Pandor3 <thatdumbphilosopher@gmail.com>
2024-10-01 11:50:00 +0000

    Added README file, exercise 0, and its main file

---
+++ python-serialization/README.md
@@ -0,0 +1,3 @@
+This repository has been made during my cursus at the Holberton coding school, it will be used to store
+various exercises which will help me better understand about serialization in the Python programming
+language.
---
+++ python-serialization/task_00_basic_serialization.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+This module will be used to contain a basic serialization which will
+add the functionality to serialize a Python dictionary to a JSON file
+aswell as deserialize a JSON file to recreate a Python dictionary
+"""
+
+
+import pickle
+
+
+def serialize_and_save_to_file(data, filename):
+    """
+    Defined function which will serialize
+    and save a Python dictionary to a JSON file
+    """
+
+    with open(filename, 'wb') as file:
+        pickle.dump(data, file)
+    pass
+
+
+def load_and_deserialize(filename):
+    """
+    Defined function which will deserialize a JSON file
+    to recreate a Python dictionary
+    """
+
+    with open(filename, 'rb') as file:
+        data = pickle.load(file)
+    return data
+    pass
