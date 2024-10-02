9d7bff4d86c280ea9ef82c56a8be54039178a1f5
Pandor3 <thatdumbphilosopher@gmail.com>
2024-10-02 09:25:07 +0000

    Added exercise 3 and its main file

---
+++ python-serialization/task_03_xml.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+This module will be used to contain 2 functions which will
+- Take a python dictionary and a filename as parameters which
+will serialize the dictionary into XML and save it.
+- Take a filename as its parameter, read the XML data from that
+and return a deserialized Python dictionary
+"""
+
+import xml.etree.ElementTree as ET
+
+
+def serialize_to_xml(dictionary, filename):
+    """
+    Defined function which will serialize a dictionary
+    into XML and save it
+    """
+
+    root = ET.Element("data")
+    for key, value in dictionary.items():
+        element = ET.SubElement(root, key)
+        element.text = str(value)
+    tree = ET.ElementTree(root)
+    tree.write(filename)
+
+
+def deserialize_from_xml(filename):
+    """
+    Defined function which will read a XML data and
+    return a deserialized Python dictionary
+    """
+
+    tree = ET.parse(filename)
+    root = tree.getroot()
+    for child in root:
+        print(child.tag, child.text)
+    return filename
