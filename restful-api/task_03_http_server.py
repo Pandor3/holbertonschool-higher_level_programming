1cece06756a4d8bbfd452fb2831d4938ec7bda1f
Pandor3 <thatdumbphilosopher@gmail.com>
2024-10-14 10:51:51 +0000

    Forgot to change a string to bytes

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -50,7 +50,7 @@ class SimpleHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
             self.send_response(404)
             self.send_header("Content-type", "text/plain")
             self.end_headers()
-            self.wfile.write("Not Found")
+            self.wfile.write(b"Not Found")
 
 
 PORT = 8000
