6584bcfa235ed8de70e436151a05a78768b7592a
Pandor3 <thatdumbphilosopher@gmail.com>
2024-10-14 10:48:46 +0000

    Edited exercise 3 because of the overindentation

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -9,11 +9,13 @@ import http.server
 
 
 class SimpleHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
+
     """
     Class which will define how the API works
     """
 
     def do_GET(self):
+
         if self.path == "/":
             self.send_response(200)
             self.send_header("Content-type", "text/plain")
@@ -24,19 +26,19 @@ class SimpleHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
             self.send_header("Content-type", "application/json")
             self.end_headers()
             data = {
-                    "name": "John",
-                    "age": "30",
-                    "city": "New York"
-                    }
+                "name": "John",
+                "age": "30",
+                "city": "New York"
+                }
             self.wfile.write(json.dumps(data).encode('utf-8'))
         elif self.path == "/info":
             self.send_response(200)
             self.send_header("Content-type", "application/json")
             self.end_headers()
             info_data = {
-                    "version": "1.0",
-                    "description": "A simple API built with http.server"
-                    }
+                "version": "1.0",
+                "description": "A simple API built with http.server"
+                }
             json_bytes = json.dumps(info_data).encode("utf-8")
             self.wfile.write(json_bytes)
         elif self.path == "/status":
