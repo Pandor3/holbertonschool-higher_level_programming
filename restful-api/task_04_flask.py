085e2626b0cfbf605334f3b130094b396ffea3d3
Pandor3 <thatdumbphilosopher@gmail.com>
2024-10-15 11:25:10 +0000

    Emptied the users dictionnary

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -11,10 +11,7 @@ from flask import jsonify
 
 app = Flask(__name__)
 
-users = {
-    "jane": {"username": "jane", "name": "Jane", "age": 28, "city": "Los Angeles"},
-    "john": {"username": "john", "name": "John", "age": 30, "city": "New York"}
-    }
+users = {}
 
 
 @app.route("/")
