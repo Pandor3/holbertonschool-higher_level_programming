f2f7d55c9056ce7beee6cd0c17d6c7b0e1f080b2
Pandor3 <thatdumbphilosopher@gmail.com>
2024-10-15 15:31:55 +0000

    Added exercise 5

--- restful-api/task_05_basic_security.py
+++ restful-api/task_05_basic_security.py
@@ -7,9 +7,13 @@ Security and authentication techniques
 from flask import Flask, request, jsonify
 from flask_httpauth import HTTPBasicAuth
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_jwt_extended import create_access_token, get_jwt_identity
+from flask_jwt_extended import jwt_required, JWTManager
 
 app = Flask(__name__)
 auth = HTTPBasicAuth()
+app.config["JWT_SECRET_KEY"] = "ABC&abc&123"
+jwt = JWTManager(app)
 
 users = {
     "user1":
@@ -26,6 +30,14 @@ users = {
         }
   }
 
+
+@app.route("/login", methods=["POST"])
+def login():
+    username = request.json.get("username", None)
+    password = request.json.get("password", None)
+
+    access_token = create_access_tokent(identity=username)
+    return jsonify(access_token=access_token)
 
 @auth.verify_password
 def verifying_password():
