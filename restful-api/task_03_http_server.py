d1514439b23e0c84e11d58dfd98b9cd5ab8555b7
Pandor3 <ThatDumbPhilosopher@gmail.com>
2024-10-15 09:07:22 +0200

    Updated task_03_http_server.py, changed double quotes by single quotes

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -16,14 +16,14 @@ class SimpleHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
 
     def do_GET(self):
 
-        if self.path == "/":
+        if self.path == '/':
             self.send_response(200)
-            self.send_header("Content-type", "text/plain")
+            self.send_header('Content-type', 'text/plain')
             self.end_headers()
-            self.wfile.write(b"Hello, this is a simple API!")
-        elif self.path == "/data":
+            self.wfile.write(b'Hello, this is a simple API!')
+        elif self.path == '/data':
             self.send_response(200)
-            self.send_header("Content-type", "application/json")
+            self.send_header('Content-type', 'application/json')
             self.end_headers()
             data = {
                     "name": "John",
@@ -31,9 +31,9 @@ class SimpleHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
                     "city": "New York"
                     }
             self.wfile.write(json.dumps(data).encode("utf-8"))
-        elif self.path == "/info":
+        elif self.path == '/info':
             self.send_response(200)
-            self.send_header("Content-type", "application/json")
+            self.send_header('Content-type', 'application/json')
             self.end_headers()
             info_data = {
                     "version": "1.0",
@@ -41,16 +41,16 @@ class SimpleHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
                     }
             json_bytes = json.dumps(info_data).encode("utf-8")
             self.wfile.write(json_bytes)
-        elif self.path == "/status":
+        elif self.path == '/status':
             self.send_response(200)
-            self.send_header("Content-type", "text/plain")
+            self.send_header('Content-type', 'text/plain')
             self.end_headers()
-            self.wfile.write(b"OK")
+            self.wfile.write(b'OK')
         else:
             self.send_response(404)
-            self.send_header("Content-type", "text/plain")
+            self.send_header('Content-type', 'text/plain')
             self.end_headers()
-            self.wfile.write(b"Endpoint not found")
+            self.wfile.write(b'Endpoint not found')
 
 
 PORT = 8000
