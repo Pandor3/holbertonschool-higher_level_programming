5427fb66a7233d57a9ada6f7651c2fe96c91c34b
Pandor3 <ThatDumbPhilosopher@gmail.com>
2024-10-29 14:47:01 +0100

    Added exercise 7

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -1,6 +1,8 @@
 -- 7. This script will create a database named hbtn_0d_usa and a table named cities
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS cities (
-    id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    state_id INT NOT NULL, FOREIGN KEY(state_id) REFERENCES states(id),
     name VARCHAR(256) NOT NULL
 );
