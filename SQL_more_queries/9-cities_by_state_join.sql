add1d1bed03d83052b4ae087ef7457db413465a5
Pandor3 <ThatDumbPhilosopher@gmail.com>
2024-10-29 15:20:48 +0100

    Maybe with chatgpt ?

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,5 @@
 -- 9. This script will list all cities contained in the database named hbtn_0d_usa
 SELECT cities.id, cities.name, states.name 
 FROM cities, states 
-WHERE cities.state_id = states_id 
+WHERE cities.state_id = states.id 
 ORDER BY cities.id ASC;
