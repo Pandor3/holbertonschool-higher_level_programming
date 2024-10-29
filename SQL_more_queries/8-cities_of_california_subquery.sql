9947aa19e50d43b38263ca3476ee748f9172af24
Pandor3 <ThatDumbPhilosopher@gmail.com>
2024-10-29 14:58:42 +0100

    Edited exercise 8's syntax

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,2 +1,2 @@
 -- 8. This script will list all the cities of California that can be found in the database named hbtn_0d_usa
-SELECT id, name FROM cities ORDER BY cities.id ASC;
+SELECT id, name FROM cities WHERE state_id = (SELECT id FROM states WHERE name = 'California') ORDER BY cities.id ASC;
