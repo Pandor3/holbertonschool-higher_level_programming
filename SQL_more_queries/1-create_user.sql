5313c3ba8e4fdd8490357f7159b68b62d7122dde
Pandor3 <ThatDumbPhilosopher@gmail.com>
2024-10-29 10:11:44 +0100

    ...And Again

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,3 +1,3 @@
 -- 1. This script will create a server user named user_0d_1
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT CREATE, ALTER, DROP, INSERT, UPDATE, DELETE, SELECT, REFERENCES, RELOAD on *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+GRANT CREATE, ALTER, DROP, INSERT, UPDATE, DELETE, SELECT on *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
