8fb5da2ca873409b8885574f1ff76f68e4aa1540
Pandor3 <ThatDumbPhilosopher@gmail.com>
2024-10-29 10:05:30 +0100

    Changed syntax on exercise 0 and added exercise 1

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
 -- 0. This script will list all privileges of the users.
-SHOW GRANTS FOR user_0d_1@localhost; 
-SHOW GRANTS FOR user_0d_2@localhost;
+SHOW GRANTS FOR 'user_0d_1'@'localhost'; 
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
