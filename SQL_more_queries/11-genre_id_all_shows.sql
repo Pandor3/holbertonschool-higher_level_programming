c8bc3bc67919ee4d047f9dd09fcfc18a6fd4700a
Pandor3 <ThatDumbPhilosopher@gmail.com>
2024-10-30 09:41:20 +0100

    Added exercise 11

---
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,5 @@
+-- 11. This script will list all shows contained in the database named hbtn_0d_tvshows
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.genre_id
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
