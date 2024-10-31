422779cd42916a22af2dd428d31ad4c0223055f4
Pandor3 <thatdumbphilosopher@gmail.com>
2024-10-31 16:22:39 +0000

    Had enough.

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -8,20 +8,19 @@ all cities of that state using the hbtn_0e_4_usa database
 import MySQLdb
 import sys
 
-
 if __name__ == "__main__":
     db = MySQLdb.connect(
-            host="localhost",
-            user=sys.argv[1],
-            password=sys.argv[2],
-            database=sys.argv[3]
+        host="localhost",
+        user=sys.argv[1],
+        password=sys.argv[2],
+        database=sys.argv[3]
     )
 
-    cursor = db.cursor
-    cursor.execute("SELECT cities.id, cities.name, states.name \
-            FROM cities JOIN states ON cities.state_id = states.id \
-            WHERE states.name = %s \
-            ORDER BY cities.id ASC;", (sys.argv[4],))
+    cursor = db.cursor()
+    cursor.execute("SELECT cities.name \
+                    FROM cities JOIN states ON cities.state_id = states.id \
+                    WHERE states.name = %s \
+                    ORDER BY cities.id ASC;", (sys.argv[4], ))
 
     print(", ".join([row[0] for row in cursor.fetchall()]))
 
