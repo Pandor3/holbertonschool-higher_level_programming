aea739ceb4df21bf9cff2fe2757f998c16658c40
Pandor3 <thatdumbphilosopher@gmail.com>
2024-10-31 15:56:22 +0000

    Added exercise 4

---
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+This module will be used to list all cities from the database
+named hbtn_0e_4_usa
+"""
+
+
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+            host="localhost",
+            user=sys.argv[1],
+            password=sys.argv[2],
+            database=sys.argv[3]
+    )
+
+    cursor = db.cursor()
+
+    cursor.execute(
+            "SELECT cities FROM states "
+            "ORDER BY cities.id ASC ",
+    )
+
+    for state in cursor.fetchall():
+        print("{}".format(state))
+
+    cursor.close()
+    db.close()
