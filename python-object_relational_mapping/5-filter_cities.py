3666898a8af412bd310bf4a2f8c844b5edf04b8d
Pandor3 <thatdumbphilosopher@gmail.com>
2024-10-31 16:35:50 +0000

    Ok

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
     db = MySQLdb.connect(
             host="localhost",
             user=sys.argv[1],
-            passwd=sys.argv[2],
+            password=sys.argv[2],
             database=sys.argv[3]
     )
 
