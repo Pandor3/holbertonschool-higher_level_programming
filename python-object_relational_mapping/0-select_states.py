0c0ea7d10b6aa9934ad7118404d9f34687060851
Pandor3 <thatdumbphilosopher@gmail.com>
2024-10-31 10:21:02 +0000

    Edited exercise 0 due to phantom characters

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,25 +1,22 @@
-!#/usr/bin/python3
-'''
-This module will be used to lists all states from the database hbtn_0e_0_usa
-'''
+#!/usr/bin/python3
+"""
+This module will lists all states from the database hbtn_0e_0_usa
+"""
 
 import MySQLdb
 import sys
 
 if __name__ == "__main__":
     db = MySQLdb.connect(
-        host="localhost", 
-        port=3306,
+        host="localhost",
         user=sys.argv[1],
         password=sys.argv[2],
         database=sys.argv[3]
-)
+    )
+
     cursor = db.cursor()
     cursor.execute("SELECT * FROM states ORDER BY states.id ASC;")
-    '''
-    Execute this SQL command
-    '''
-    
+
     for state in cursor.fetchall():
         print(state)
 
