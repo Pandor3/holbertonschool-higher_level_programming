33597a2a1deae277281ecc67181c1005b25a1715
Pandor3 <ThatDumbPhilosopher@gmail.com>
2024-10-31 11:10:28 +0100

    Used dos2unix to convert the exercise file

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -16,6 +16,9 @@ if __name__ == "__main__":
 )
     cursor = db.cursor()
     cursor.execute("SELECT * FROM states ORDER BY states.id ASC;")
+    '''
+    Execute this SQL command
+    '''
     
     for state in cursor.fetchall():
         print(state)
