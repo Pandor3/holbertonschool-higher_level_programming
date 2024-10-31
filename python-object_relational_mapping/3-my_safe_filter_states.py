62750cbde1594482d4ec428aa02efde818b61e43
Pandor3 <thatdumbphilosopher@gmail.com>
2024-10-31 15:47:26 +0000

    Why do you read this ?

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -11,14 +11,15 @@ if __name__ == "__main__":
     db = MySQLdb.connect(
         host="localhost",
         user=sys.argv[1],
-        passwd=sys.argv[2],
-        db=sys.argv[3]
+        password=sys.argv[2],
+        database=sys.argv[3]
     )
 
     cursor = db.cursor()
 
     cursor.execute(
         "SELECT * FROM states "
+        "WHERE name = %s "
         "ORDER BY id ASC",
         (sys.argv[4],)
     )
