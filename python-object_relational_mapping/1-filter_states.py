17fcb2c87f8191c57a1860fa3bfdf1d27d212750
Pandor3 <thatdumbphilosopher@gmail.com>
2024-10-31 14:53:02 +0000

    Edited exercise 1 syntax

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -11,7 +11,7 @@ import sys
 if __name__ == "__main__":
     db = MySQLdb.connect(
         host="localhost",
-        username=sys.argv[1],
+        user=sys.argv[1],
         password=sys.argv[2],
         database=sys.argv[3]
     )
