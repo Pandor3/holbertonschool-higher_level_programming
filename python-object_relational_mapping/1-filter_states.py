70cdad820e8d1b97f7fec7468f5f35a37486eb31
Pandor3 <thatdumbphilosopher@gmail.com>
2024-10-31 14:38:50 +0000

    Added exercise 1

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+This module will lists all states with a name starting with
+N (upper N) from the database hbtn_0e_0_usa
+"""
+
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        host="localhost",
+        username=sys.argv[1],
+        password=sys.argv[2],
+        database=sys.argv[3]
+    )
+
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states.id ORDER BY states.id ASC;")
+
+    for state.id in cursor.fetchall():
+        print(state.id)
+
+    cursor.close()
+    db.close()
