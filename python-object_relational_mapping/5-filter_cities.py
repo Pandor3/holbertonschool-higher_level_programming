cebb3d6015399d0fd4b05439dc3c0e00d41fd868
Pandor3 <thatdumbphilosopher@gmail.com>
2024-10-31 16:34:48 +0000

    Again ?

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -13,13 +13,13 @@ if __name__ == "__main__":
     db = MySQLdb.connect(
             host="localhost",
             user=sys.argv[1],
-            password=sys.argv[2],
+            passwd=sys.argv[2],
             database=sys.argv[3]
     )
 
     cursor = db.cursor()
 
-    cursor.execute("SELECT cities.id, cities.name, states.name \
+    cursor.execute("SELECT cities.name \
             FROM cities JOIN states ON cities.state_id = states.id \
             WHERE states.name = %s \
             ORDER BY cities.id ASC;", (sys.argv[4],))
